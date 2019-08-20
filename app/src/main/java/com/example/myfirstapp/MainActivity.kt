bfca5166109dd0011175149aaee20130172c0fe1
David-Zhang0238 <xi0238@163.com>
2019-08-20 17:40:16 +0800

    我的这次修改，请大家注意。

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    //张成超写代码
+    //张成超写代码（我的这次修改，请大家注意。）
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
