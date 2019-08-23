9f421e0449e8cbdbf0703faf255624ac714b0795
David-Zhang0238 <xi0238@163.com>
2019-08-23 16:00:02 +0800

    我的这次修改，请大家注意，哈哈。

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     //Written by David Zhang  用来控制灌溉
     override fun onCreate(savedInstanceState: Bundle?) {
+        //集成父类
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
